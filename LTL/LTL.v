9a7ecc90771fc75f83dc58ad9714e43487bba768
SatyendraBanjare <satyendrabanjare99@gmail.com>
2019-07-20 14:09:38 +0530

    updated definitions

--- LTL/LTL.v
+++ LTL/LTL.v
@@ -1,6 +1,7 @@
 Require Import Stream.
 Require Import Coq.Logic.Classical Omega.
 
+
 (*
 
 This section contains the different definitions to be used for
@@ -14,57 +15,57 @@ WeakUntil , StrongRelease.
 
 *)
 
-Definition LTLProp (A : Type) : Type := Stream A -> Prop.
+Definition Stream_State (A : Type) : Type := Stream A -> Prop.
 
 Section LTL.
   Variable A : Type.
 
-  Inductive Atomic (At : A -> Prop) : LTLProp A :=
+  Inductive Atomic (At : A -> Prop) : Stream_State A :=
   | AtomicIntro : forall (a : A) (l : Stream A),
       At a -> Atomic At (Cons a l).
 
-  Definition And (P Q : LTLProp A) : LTLProp A :=
+  Definition And (P Q : Stream_State A) : Stream_State A :=
     fun l => P l /\ Q l.
 
-  Definition Or (P Q : LTLProp A) : LTLProp A :=
+  Definition Or (P Q : Stream_State A) : Stream_State A :=
     fun l => P l \/ Q l.
 
-  Definition Not (P : LTLProp A) : LTLProp A :=
+  Definition Not (P : Stream_State A) : Stream_State A :=
     fun l => ~ (P l).
 
-  Inductive Next (P : LTLProp A) : LTLProp A :=
+  Inductive Next (P : Stream_State A) : Stream_State A :=
   | NextIntro : forall (a : A) (l : Stream A),
       P l -> Next P (Cons a l).
 
-  Inductive Until (P Q : LTLProp A) : LTLProp A :=
+  Inductive Until (P Q : Stream_State A) : Stream_State A :=
   | UntilHere : forall (l : Stream A),
       Q l -> Until P Q l
   | UntilLater : forall (a : A) (l : Stream A),
       P (Cons a l) -> Until P Q l -> Until P Q (Cons a l).
 
-  CoInductive Release (P Q : LTLProp A) : LTLProp A :=
+  CoInductive Release (P Q : Stream_State A) : Stream_State A :=
   | ReleaseHere : forall (l : Stream A),
       P l -> Q l -> Release P Q l
   | ReleaseLater : forall (a : A) (l : Stream A),
       Q (Cons a l) -> Release P Q l -> Release P Q (Cons a l).
 
-  Inductive Eventually (P : LTLProp A) : LTLProp A :=
+  Inductive Eventually (P : Stream_State A) : Stream_State A :=
   | EventuallyHere : forall (a : A) (l : Stream A),
       P (Cons a l) -> Eventually P (Cons a l)
   | EventuallyLater : forall (a : A) (l : Stream A),
       Eventually P l -> Eventually P (Cons a l).
 
-  CoInductive Always (P : LTLProp A) : LTLProp A :=
+  CoInductive Always (P : Stream_State A) : Stream_State A :=
   | AlwaysIntro : forall (a : A) (l : Stream A),
       P (Cons a l) -> Always P l -> Always P (Cons a l).
 
-  CoInductive WeakUntil (P Q: LTLProp A) : LTLProp A :=
+  CoInductive WeakUntil (P Q: Stream_State A) : Stream_State A :=
   | WeakUntilHere : forall (l : Stream A),
     Q l -> WeakUntil P Q l
   | WeakUntilLater : forall (a : A) (l : Stream A),
     Q (Cons a l) -> WeakUntil P Q l -> WeakUntil P Q (Cons a l).
 
-  CoInductive StrongRelease (P Q: LTLProp A) : LTLProp A :=
+  CoInductive StrongRelease (P Q: Stream_State A) : Stream_State A :=
   | StrongReleaseHere : forall (l : Stream A),
     P l -> Q l -> StrongRelease P Q l
   | StrongReleaseLater : forall (a : A) (l : Stream A),
